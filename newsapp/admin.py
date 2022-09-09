749b12041d1e078a876cea2e7647a5ca4553718c
Amr Atif <62966382+amr-atif@users.noreply.github.com>
2022-09-09 14:34:37 +0200

    create newsapp

---
+++ newsapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import News
+
+# Register your models here.
+
+admin.site.register(News)
---
+++ newsapp/models.py
@@ -0,0 +1,22 @@
+from pyexpat import model
+from re import T
+from tkinter.tix import Tree
+from django.db import models
+from doctors.models import Doctor
+from django.urls import reverse
+# Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    pic = models.ImageField()
+    newsdate = models.DateField(null=True, blank=True)
+    docname = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('new_detail', kwargs={'pk': self.pk})
