01f00c54f4e2562f78e9780098fc201d799351a7
Amr Atif <62966382+amr-atif@users.noreply.github.com>
2022-09-14 19:50:42 +0200

    make email unique

---
+++ pages/templatetags/checker.py
@@ -0,0 +1,8 @@
+from atexit import register
+from django import template
+register=template.Library()
+
+@register.simple_tag 
+def  update_variable(value):
+    """Allows to update existing variable in template""" 
+    return value
